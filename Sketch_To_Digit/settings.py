b2ff5ea22f13bfe1ff2894a70beb7f9d45a567a6
slayeh17 <debakroy7@gmail.com>
2024-02-08 19:53:55 +0530

    add stuffs for vercel

--- Sketch_To_Digit/settings.py
+++ Sketch_To_Digit/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w*uiexozdy=k%ugr-t@_-^!pfp(wrz@wn=#r(16y%unv#l)%0@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
